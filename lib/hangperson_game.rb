3b76090c588824f58377e54569e576ff6f7cf691
EC2 Default User <ec2-user@ip-172-31-39-166.us-east-2.compute.internal>
2019-02-03 18:55:04 +0000

    displayed word with guesses passed

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -30,6 +30,16 @@ class HangpersonGame
       false
     end
   end
+  
+  def word_with_guesses
+    result = ''
+    @word.split('').each do |letter|
+      result << letter if @guesses.include? letter
+      result << '-' if ! @guesses.include? letter
+    end
+    result
+  end
+    
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
